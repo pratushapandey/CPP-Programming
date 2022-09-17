3bd8d10d743bbca6f157437ad3e3d1a4a07e3c6b
Pratusha Pandey <pratushapandey08@gmail.com>
2022-09-17 23:18:09 +0530

    Add files via upload

---
+++ VotingSystem.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int age;
+	
+	cout<<"Enter your age : "<<endl;
+	cin>>age;
+	
+	if(age >= 18){
+		cout<<"Vote" <<endl;
+	}
+	else{
+		cout<<"Can not Vote";
+	}
+	return 0;
+}
