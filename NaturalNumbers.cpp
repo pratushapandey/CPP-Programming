6f9f78c20d5154888dde5543cdb716f7107c1e09
Pratusha Pandey <pratushapandey08@gmail.com>
2022-09-17 23:14:57 +0530

    Add files via upload

---
+++ NaturalNumbers.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n,i;
+	cout<<"Enter the range of natural numbers :"<<endl;
+	cin>>n;
+	
+	cout<<"The natural numbers are :" << endl;
+	for(i = 1; i <= n; i++){
+		cout<<i<<"\t";
+	}
+	return 0;
+}
