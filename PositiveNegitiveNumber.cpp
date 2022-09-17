e9f7fd4b4c61b9b3fcefd1759f2f9b44e163f24b
Pratusha Pandey <pratushapandey08@gmail.com>
2022-09-17 23:15:31 +0530

    Add files via upload

---
+++ PositiveNegitiveNumber.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int num;
+	
+	cout<<"Enter the number : ";
+	cin >> num;
+	
+	if(num==0)
+	cout<<"Zero"; else(num > 0) ? cout << "Positive Number" : cout <<"Negitive Number";
+	
+	return 0;
+}
