add89598e9f88c46c759984f5595eb9f83aa6302
Pratusha Pandey <pratushapandey08@gmail.com>
2022-10-10 11:32:22 +0530

    Add files via upload

--- Pattern10.cpp
+++ Pattern10.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main(){
 	int n;
-	cin>>n;
+	cin>>n; //3
 	int i = 1;
-	while(i <= n){ // 1 < 4 , 2 < 4 , 3 < 4 , 4 <= 4
-		int j = i; // j = 1 , 2 , 3 , 4
-		while(j < i*2){  // 1 < 2 , 2 < 4 , 3 < 6 , 4 < 8
-			cout<<j<<" "; // j = 1 , j= 2,3 , j= 3,4,5 , j= 4,5,6,7
-			j++; // 2 , 3 , 4
+	while(i <= n){ // 1 <= 3 , 2 <= 3
+		int j = 1;
+		while(j <= i){   // 1 <= 1 , 1,2 <= 2
+			cout<<(i - j + 1)<<" "; // 1 , 2-1
+			j++; //2
 		}
 		cout<<endl;
-		i++; // 2 , 3 , 4
+		i++; // 2
 	}
 	return 0;
 }
