b68157d659511b85bbb761dc13afb62f6c8bc57c
Pratusha Pandey <pratushapandey08@gmail.com>
2022-10-11 18:14:33 +0530

    Add files via upload

---
+++ Pattern11.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int n;
+	cin >> n;
+	int i = 0;
+	while(i < n){
+		int j = 0;
+		while(j < n){
+			cout << (char)(65 + j)<<" ";
+			j++;
+		}
+		cout<<endl;
+		i++;
+	}
+}
