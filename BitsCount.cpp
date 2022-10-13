9f55cff6f373a8033bcac5e3074a0569a603c90a
Pratusha Pandey <pratushapandey08@gmail.com>
2022-10-13 22:15:41 +0530

    Add files via upload

---
+++ BitsCount.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int bits(int a){
+	int count = 0;
+	
+	while(a){
+		count += a & 1;
+		a= a >> 1;
+	}
+	return count;
+}
+
+int main(){
+	int a;
+	
+	cin >> a;
+	cout<<bits(a)<<endl;
+	
+	return 0;
+}
