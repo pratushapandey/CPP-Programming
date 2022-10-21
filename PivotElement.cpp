fc28a623515a470030d07e6a989f77e554cf68f1
Pratusha Pandey <pratushapandey08@gmail.com>
2022-10-21 21:17:02 +0530

    Add files via upload

--- PivotElement.cpp
+++ PivotElement.cpp
@@ -1,27 +1,24 @@
-#include<iostream> 
+#include<iostream>
 using namespace std;
 
-int getPivot(int arr[], int n) {
+int getPivot(int arr[],int n){
+	int low = 0, high =n-1;
+	
+	while(low < high){
+		int mid = low + (high-low)/2;
+		
+		if(arr[mid] >= arr[0])
+		low = mid+1;
+		
+		else
+		high = mid;
+	}
+	return low;
+}	
 
-    int s = 0;
-    int e = n-1;
-    int mid = s + (e-s)/2;
 
-    while(s<e) {
-
-        if(arr[mid] >= arr[0])
-        {
-            s = mid+1;
-        }
-        else{
-            e = mid;
-        }
-        mid = s + (e-s)/2;
-    }
-    return s;
-}
-
-int main() {
-    int arr[6] = {1,7,3,6,5,6};
-    cout << "Pivot is " << getPivot(arr, 6) << endl;
+int main(){
+	int arr[5] ={8,10,17,1,3};
+	
+	cout<<"Pivot is : "<<getPivot(arr,5)<<endl;
 }
